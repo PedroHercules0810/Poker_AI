09aa77605d11c89ab90576c9facae05762306ea0
angellusj <angela..souza@alunos.ufersa.edu.br>
2024-08-13 09:57:28 -0300

    começando a função croupier

---
+++ src/croupier.py
@@ -0,0 +1,7 @@
+def _croupier_(){
+    #Criando as arrays que serviram como o baralho
+    ouro = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+    copas = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+    espadas = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+    paus = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+}
