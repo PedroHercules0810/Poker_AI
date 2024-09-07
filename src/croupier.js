ff41f995fe6234ce511d279f861c934d2cb47de5
PedroHercules0810 <pedrosouzagrangeiro@gmail.com>
2024-09-07 09:54:09 -0300

    Fazendo o croupier distribuir as cartas comunitarias

--- src/croupier.js
+++ src/croupier.js
@@ -1,96 +1,91 @@
-//função que escolhe valores aleatorios dentro de um vetor
+var cartasUsadas = [];
+const naipes = ['O', 'E', 'P', 'C'];
+const valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let numero_jogadores = 10;
+let jogadores = [];
+
 function escolherValorAleatorio(vetor) {
     const indiceAleatorio = Math.floor(Math.random() * vetor.length);
     return vetor[indiceAleatorio];
 }
 
-//classe para criação das cartas
 class Carta {
     constructor(naipe, valor) {
-        this.naipe = naipe;
-        this.valor = valor;
-
-        const naipes = ['O', 'E', 'P', 'C'];
-        const valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
-        //aleatorizando tanto o naipe quanto o valor
-        const naipe_aleatorio = escolherValorAleatorio(naipes)
-        let valor_aleatorio = escolherValorAleatorio(valores)
-
-        /*
-        if (valor_aleatorio == 11) {
-            valor_aleatorio = "Valete"
-        }
-        if (valor_aleatorio == 12) {
-            valor_aleatorio = "Dama"
-        }
-        if (valor_aleatorio == 13) {
-            valor_aleatorio = "Rei"
-        }
-        if (valor_aleatorio == 14) {
-            valor_aleatorio = "Ás"
-        }
-        */
-        //gerando a carta propriamente dita
-        const carta = [valor_aleatorio, naipe_aleatorio];
-
-        return carta;
+        this.naipe = escolherValorAleatorio(naipes);
+        this.valor = escolherValorAleatorio(valores);
     }
 }
 
 class Jogador {
-    constructor(carta_1, carta_2) {
+    constructor( carta_1, carta_2) {
         this.carta_1 = new Carta;
         this.carta_2 = new Carta;
-
-        if (carta_1 == carta_2) {
-            this.carta_1 = new Carta;
-            this.carta_2 = new Carta;
-        }
     }
 }
 
+function cartasSaoIguais(carta1, carta2) {
+    return carta1.valor === carta2.valor && carta1.naipe === carta2.naipe;
+}
 
-function diferenciar_jogos(jogadores) {
-    for (let j = 0; j < jogadores.length; j++) {
-        for (let k = 0; k < jogadores.length; k++) {
-            if (j != 0 && k != 0 && jogadores[j].carta_1 == jogadores[k].carta_1) {
-                jogadores[j].carta_1 = new Carta;
-            }
-        }
-
-        for (let m = 0; m < jogadores.length; m++) {
-            if (j != 0 && m != 0 && jogadores[j].carta_2 == jogadores[m].carta_2) {
-                jogadores[j].carta_2 = new Carta;
-            }
-        }
-        for (let l = 0; l < jogadores.length; l++) {
-            if (j != 0 && l != 0 && jogadores[j].carta_1 == jogadores[l].carta_2) {
-                jogadores[j].carta_1 = new Carta;
-            }
-        }
-        for (let n = 0; n < jogadores.length; n++) {
-            if (j != 0 && n != 0 && jogadores[j].carta_2 == jogadores[n].carta_1) {
-                jogadores[j].carta_2 = new Carta;
-            }
-        }
+function  distribuirCartas(){
+    let c1 = new Carta();
+    while (cartasUsadas.some(carta => cartasSaoIguais(carta, c1))) {
+        c1 = new Carta();
     }
+    cartasUsadas.push(c1);
+    let c2 = new Carta();
+    while (cartasUsadas.some(carta => cartasSaoIguais(carta, c2))) {
+        c2 = new Carta();
+    }
+    cartasUsadas.push(c2);
 
-    return jogadores;
+    let jog = new Jogador();
+    jog.carta_1 = c1;
+    jog.carta_2 = c2;
+    jogadores.push(jog);
+}
+function  distribuirCartasComunitarias(){
+    let c1 = new Carta();
+    while (cartasUsadas.some(carta => cartasSaoIguais(carta, c1))) {
+        c1 = new Carta();
+    }
+    cartasUsadas.push(c1);
+    let c2 = new Carta();
+    while (cartasUsadas.some(carta => cartasSaoIguais(carta, c2))) {
+        c2 = new Carta();
+    }
+    cartasUsadas.push(c2);
+    let c3 = new Carta();
+    while (cartasUsadas.some(carta => cartasSaoIguais(carta, c3))) {
+        c3 = new Carta();
+    }
+    cartasUsadas.push(c3);
+    let c4 = new Carta();
+    while (cartasUsadas.some(carta => cartasSaoIguais(carta, c4))) {
+        c4 = new Carta();
+    }
+    cartasUsadas.push(c4);
+    let c5 = new Carta();
+    while (cartasUsadas.some(carta => cartasSaoIguais(carta, c5))) {
+        c5 = new Carta();
+    }
+    cartasUsadas.push(c5);
+
+    let comunitarias = [c1,c2,c3,c4,c5];
+
+    return comunitarias;
+
 }
 
+console.log(distribuirCartasComunitarias());
 
 
-
-let numero_jogadores = 10;
-let jogadores = [];
-
-for (let i = 0; i < numero_jogadores; i++) {
-    const jogador = new Jogador();
-    jogadores.push(jogador);
+for(let i=0;i<numero_jogadores; i++){
+    distribuirCartas();
 }
 
+for(let i=0;i<numero_jogadores; i++){
+    console.log(jogadores[i]);
+}
 
-
-console.log(diferenciar_jogos(jogadores));
 
